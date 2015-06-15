24d9b40a688b57d76c7dcc27c3114ec2697936b3
9drashijin <9drashijin@live.com>
2015-06-15 12:00:17 +0800

    taskBLOCK
    Signed-off-by: 9drashijin <9drashijin@live.com>

---
+++ app/taskBlock.h
@@ -0,0 +1,20 @@
+#ifndef Task_Block_H
+#define Task_Block_H
+
+#include <stdint.h>
+
+typedef enum {
+  RUNNING,
+  READY,
+  SPAWNED,
+  TERMINATED,
+  BLOCKING,
+  SLEEPING
+}TaskState;
+
+typedef struct {
+  TaskState state;
+  uint32_t Stack;
+}Tcb;
+
+#endif // Task_Block_H
